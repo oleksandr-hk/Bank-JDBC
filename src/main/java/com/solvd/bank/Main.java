59593efc0c73e484f842ce45cd20e984e13cc9df
Alexander <alexanderpeterburg21@gmail.com>
2019-07-29 21:47:39 +0300

    XML some fixes

--- src/main/java/com/solvd/bank/Main.java
+++ src/main/java/com/solvd/bank/Main.java
@@ -12,17 +12,7 @@ public class Main {
 
     public static void main(String args[]){
 
-
-        try {
-            List<Bank> bankList = XMLReader.parseXML();
-            System.out.println(bankList.size());
-        } catch (XMLStreamException e) {
-            e.printStackTrace();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-
-
+        
 
     }
 
